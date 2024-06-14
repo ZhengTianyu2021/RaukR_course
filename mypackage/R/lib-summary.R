aee352e83439c00c1e527156a50d66cbd9d82013
ZhengTianyu2021 <zhengtianyunc@gmail.com>
2024-06-14 12:06:14 +0200

    dependency on fs

--- mypackage/R/lib-summary.R
+++ mypackage/R/lib-summary.R
@@ -12,11 +12,39 @@ lib_summary <- function() {
   ...
 }
 
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
+}
+
+lib_summary <- function(sizes = FALSE) {
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+
+  if (sizes) {
+    pkg_df$lib_size <- vapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      },
+      FUN.VALUE = numeric(1)
+    )
+  }
   pkg_df
 }
 
